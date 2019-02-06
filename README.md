ff1c0a753fc26826c811845a63ec6360bfa1d4a4
Anton Belodedenko <abelodedenko@thrivepos.com>
2019-02-06 13:32:36 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,149 @@
+# cfn-custom-resource-provider
+> Generic CloudFormation [Custom Resources](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-custom-resources.html) provider.
+
+
+## CloudFormation
+
+### init
+
+    git clone https://github.com/ab77/cfn-generic-custom-resource
+
+    git pull --recurse-submodules
+
+    git submodule update --remote --recursive
+
+
+### certificates
+> [generate](https://docs.aws.amazon.com/vpn/latest/clientvpn-admin/authentication-authrization.html)
+
+    server_certificate=$(aws acm import-certificate\
+      --certificate file://easy-rsa/easyrsa3/pki/issued/server.crt\
+      --private-key file://easy-rsa/easyrsa3/pki/private/server.key\
+      --certificate-chain file://easy-rsa/easyrsa3/pki/ca.crt | jq -r '.CertificateArn')
+
+    client_certificate=$(aws acm import-certificate\
+      --certificate file://easy-rsa/easyrsa3/pki/issued/client1.crt\
+      --private-key file://easy-rsa/easyrsa3/pki/private/client1.key\
+      --certificate-chain file://easy-rsa/easyrsa3/pki/ca.crt | jq -r '.CertificateArn')
+
+
+### intall requirements
+> Lambda provided boto3 doesn't support Client VPN resources at the time of writing
+
+    pushd generic_provider\
+      && pip install --upgrade -r requirements.txt -t .\
+      && popd
+
+
+### package assets
+> creates a new bucket with a random GUID
+
+    bucket=$(uuid)
+    aws s3 mb s3://${bucket}
+
+    for template in lambda client-vpn main; do
+        aws cloudformation package\
+          --template-file ${template}-template.yaml\
+          --s3-bucket ${bucket}\
+          --output-template-file ${template}.yaml
+    done
+
+
+### deploy stack
+
+    stack_name='client-vpn-demo'
+    vpc_id=vpc-abcdef1234567890
+    subnets=(subnet-abcdef1234567890 subnet-1234567890abcdef)
+    subnet_count=${#subnets[@]}
+    cidr=172.16.0.0/22
+
+
+    aws cloudformation deploy\
+      --template-file main.yaml\
+      --stack-name ${stack_name}\
+      --capabilities CAPABILITY_IAM\
+      --parameter-overrides\
+      VpcId=${vpc_id}\
+      CidrBlock=${cidr}\
+      SubnetIds=$(echo ${subnets[*]} | tr ' ' ',')\
+      SubnetCount=${subnet_count}\
+      ServerCertificateArn=${server_certificate}\
+      ClientRootCertificateChainArn=${client_certificate}\
+      --tags\
+      Name=${stack_name}\
+      Region=${AWS_REGION}\
+      Profile=${AWS_PROFILE}\
+      AccountId=$(aws sts get-caller-identity | jq -r '.Account')
+
+
+### download profile
+
+    vpn_stack=$(aws cloudformation list-exports\
+      | jq -r ".Exports[] | select(.Name==\"VPNStackName-${stack_name}\").Value")
+
+    client_vpn_endpoint=$(aws cloudformation list-exports\
+      | jq -r ".Exports[] | select(.Name | startswith(\"ClientVpnEndpointId-${vpn_stack}\")).Value")
+
+    aws ec2 export-client-vpn-client-configuration\
+      --client-vpn-endpoint-id ${client_vpn_endpoint} | jq -r '.ClientConfiguration' > client.ovpn
+
+
+### connect
+
+* [macOS](https://tunnelblick.net/downloads.html)
+* [Windows/Linux](https://openvpn.net/community-downloads/)
+
+
+
+## mock requests
+
+### Directory Services
+
+#### AD Connector
+> mock CloudFormation CREATE [request](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ds.html) to create [AD Connector](https://docs.aws.amazon.com/directoryservice/latest/admin-guide/directory_ad_connector.html?icmpid=docs_dirservices_console)
+
+
+    mock_lambda_event=$(echo "{
+      \"RequestType\": \"Create\",
+      \"ResponseURL\": \"https://cloudformation-custom-resource-response-${AWS_REGION}.s3.amazonaws.com/\",
+      \"StackId\": \"arn:aws:cloudformation:${AWS_REGION}:$(aws sts get-caller-identity | jq -r '.Account'):stack/MockStack/$(uuid)\",
+      \"RequestId\": \"$(uuid)\",
+      \"ResourceType\": \"Custom::MockResource\",
+      \"LogicalResourceId\": \"MockResource\",
+      \"ResourceProperties\": {
+        \"AgentService\": \"ds\",
+        \"AgentType\": \"client\",
+        \"AgentCreateMethod\": \"connect_directory\",
+        \"AgentDeleteMethod\": \"delete_directory\",
+        \"AgentWaitMethod\": \"describe_directories\",
+        \"AgentWaitQueryExpr\": \"DirectoryDescriptions[*].Stage\",
+        \"AgentWaitCreateQueryValues\": [
+            \"Active\"
+        ],
+        \"AgentWaitUpdateQueryValues\": [],
+        \"AgentWaitDeleteQueryValues\": [],
+        \"AgentResourceId\": \"DirectoryId\",
+        \"AgentWaitResourceId\": [
+          \"DirectoryIds\"
+        ],
+        \"AgentCreateArgs\": {
+          \"Size\": \"Small\",
+          \"Description\": \"Active Directory connection.\",
+          \"Name\": \"foo-bar.local\",
+          \"ShortName\": \"foo-bar\",
+          \"Password\": \"bar\",
+          \"ConnectSettings\": {
+            \"VpcId\": \"vpc-abcdef1234567890\",
+            \"SubnetIds\": [
+              \"subnet-1234567890abcdef\",
+              \"subnet-abcdef1234567890\"
+            ],
+            \"CustomerDnsIps\": [
+              \"1.2.3.4\",
+              \"4.5.6.7\"
+            ],
+            \"CustomerUserName\": \"foo\"
+          }
+        }
+      }
+    }" | jq -c) && ./generic_provider.py "${mock_lambda_event}"
---
+++ generic_provider/generic_provider.py
@@ -0,0 +1,312 @@
+#!/usr/bin/env python
+
+import cfnresponse
+import boto3
+import json
+import os
+import sys
+from uuid import uuid4
+from jsonpath import jsonpath
+from time import sleep
+from traceback import print_exc
+
+
+default_wait = 5    # seconds
+region = os.getenv('AWS_REGION')
+profile = os.getenv('AWS_PROFILE')
+
+session = boto3.session.Session()
+boto3.setup_default_session()
+if profile:
+    print('profile={} region={}'.format(profile, region))
+    session = boto3.session.Session(profile_name=profile)
+    boto3.setup_default_session(profile_name=profile)
+
+
+def wait_event(agent, event, create=False, update=False, delete=False):
+    resource_key = 'ResourceProperties'
+    if update:
+        resource_key = 'OldResourceProperties'
+        try:
+            agent_query_value = event[resource_key]['AgentWaitUpdateQueryValues']
+        except:
+            agent_query_value = None
+    if create:
+        try:
+            agent_query_value = event[resource_key]['AgentWaitCreateQueryValues']
+        except:
+            agent_query_value = None
+    if delete:
+        try:
+            agent_query_value = event[resource_key]['AgentWaitDeleteQueryValues']
+        except:
+            agent_query_value = None
+    try:
+        agent_kwargs = json.loads(event[resource_key]['AgentWaitArgs'])
+    except:
+        try:
+            agent_kwargs = event[resource_key]['AgentWaitArgs']
+        except:
+            agent_kwargs = {}
+    try:
+        agent_resource_id = event[resource_key]['AgentWaitResourceId']
+        if type(agent_resource_id) == list:
+            agent_kwargs[agent_resource_id[0]] = [event['PhysicalResourceId']]
+        else:
+            agent_kwargs[agent_resource_id] = event['PhysicalResourceId']
+    except:
+        pass
+    try:
+        agent_method = event[resource_key]['AgentWaitMethod']
+    except:
+        agent_method = None
+    try:
+        agent_query_expr = event[resource_key]['AgentWaitQueryExpr']
+    except:
+        agent_query_expr = None
+    try:
+        assert agent_method in getattr(agent, 'waiter_names')
+        waiter = getattr(agent, 'get_waiter')(agent_method)
+        agent_attr = None
+    except:
+        waiter = None
+        try:
+            agent_attr = getattr(agent, agent_method)
+        except:
+            agent_attr = None
+
+    if waiter:
+        print(
+            'agent_method={}, agent_kwargs={}, waiter={} create={} update={} delete={}'.format(
+                agent_method,
+                agent_kwargs,
+                waiter,
+                create,
+                update,
+                delete
+            )
+        )
+        waiter.wait(**agent_kwargs)
+
+    if agent_attr and agent_query_expr and agent_query_value is not None:
+        response = {}
+        match = None
+        while True:
+            response = agent_attr(**agent_kwargs)
+            match = jsonpath(response, agent_query_expr)
+            print(
+                'agent_method={}, agent_kwargs={}, agent_attr={} agent_query_expr={} agent_query_value={} match={} create={} update={} delete={}'.format(
+                    agent_method,
+                    agent_kwargs,
+                    agent_attr,
+                    agent_query_expr,
+                    agent_query_value,
+                    match,
+                    create,
+                    update,
+                    delete
+                )
+            )
+
+            if match is not None and response and (match == agent_query_value or not match): break
+            sleep(default_wait)
+
+
+def handle_event(agent, event, create=False, update=False, delete=False):
+    resource_key = 'ResourceProperties'
+    args_key = 'AgentCreateArgs'
+    method_key = 'AgentCreateMethod'
+    if update:
+        resource_key = 'OldResourceProperties'
+        args_key = 'AgentUpdateArgs'
+        method_key = 'AgentUpdateMethod'
+    if delete:
+        args_key = 'AgentDeleteArgs'
+        method_key = 'AgentDeleteMethod'
+    try:
+        agent_kwargs = json.loads(event[resource_key][args_key])
+    except:
+        try:
+            agent_kwargs = event[resource_key][args_key]
+        except:
+            agent_kwargs = {}
+    try:
+        agent_resource_id = event[resource_key]['AgentResourceId']
+    except:
+        pass
+    if update or delete:
+        try:
+            agent_kwargs[agent_resource_id] = event['PhysicalResourceId']
+        except:
+            pass
+    try:
+        agent_method = event[resource_key][method_key]
+    except:
+        agent_method = None
+    try:
+        agent_attr = getattr(agent, agent_method)
+    except:
+        print_exc()
+        agent_attr = None
+    if agent_attr:
+        response = agent_attr(**agent_kwargs)
+        print(
+            'agent_method={}, agent_kwargs={}, agent_attr={} response={} create={} update={} delete={}'.format(
+                agent_method,
+                agent_kwargs,
+                agent_attr,
+                response,
+                create,
+                update,
+                delete
+            )
+        )
+        wait_event(agent, event, create=create, update=update, delete=delete)
+        try:
+            responseData = response
+        except:
+            responseData = {}
+        try:
+            PhysicalResourceId = response[agent_resource_id]
+        except:
+            PhysicalResourceId = str(uuid4())
+        if create:
+            print(
+                'PhysicalResourceId={} responseData={}'.format(
+                    PhysicalResourceId,
+                    responseData
+                )
+            )
+            return (PhysicalResourceId, responseData)
+        else:
+            print(
+                'PhysicalResourceId={} responseData={}'.format(
+                    event['PhysicalResourceId'],
+                    responseData
+                )
+            )
+            return responseData
+
+
+def lambda_handler(event=None, context=None):
+    try:
+        print('event: {}, context: {}'.format(json.dumps(event), context))
+    except:
+        pass
+
+    kwargs = {}
+    kwargs['region_name'] = region
+
+    try:
+        RoleArn = event['ResourceProperties']['RoleArn']
+        client = boto3.client('sts', region_name=region)
+        response = client.assume_role(
+            RoleArn=RoleArn,
+            RoleSessionName=str(uuid4())
+        )
+        kwargs['aws_access_key_id'] = response['Credentials']['AccessKeyId'],
+        kwargs['aws_secret_access_key'] = response['Credentials']['SecretAccessKey'],
+        kwargs['aws_session_token'] = response['Credentials']['SessionToken']
+        print(client.get_caller_identity())
+    except:
+        if not profile:
+            kwargs['aws_access_key_id'] = os.getenv('AWS_ACCESS_KEY_ID')
+            kwargs['aws_secret_access_key'] = os.getenv('AWS_SECRET_ACCESS_KEY')
+            kwargs['aws_session_token'] = os.getenv('AWS_SESSION_TOKEN')
+
+    responseData = {}
+
+    try:
+        agent_service = event['ResourceProperties']['AgentService']
+        agent_type = event['ResourceProperties']['AgentType']
+        StackId = event['StackId']
+        ResponseURL = event['ResponseURL']
+        RequestType = event['RequestType']
+        ResourceType = event['ResourceType']
+        RequestId = event['RequestId']
+        LogicalResourceId = event['LogicalResourceId']
+        if agent_type == 'client': agent = session.client(agent_service, **kwargs)
+        if agent_type == 'resource': agent = session.resource(agent_service, **kwargs)
+    except:
+        print_exc()
+        cfnresponse.send(
+            event,
+            context,
+            cfnresponse.FAILED
+        )
+        return
+
+
+    ''' Update: runs only if AgentUpdateMethod is present otherwise the old resource is
+        deleted and a new one is created. No backups are taken, possible loss of data.'''
+    if RequestType == 'Update':
+        try:
+            responseData = handle_event(agent, event, update=True)
+            if responseData:
+                cfnresponse.send(
+                    event,
+                    context,
+                    cfnresponse.SUCCESS,
+                    responseData=responseData,
+                    physicalResourceId=event['PhysicalResourceId']
+                )
+                return
+        except:
+            print_exc()
+            cfnresponse.send(
+                event,
+                context,
+                cfnresponse.FAILED
+            )
+            return
+
+
+    ''' Delete: runs if AgentDeleteMethod is present. Returns immediatly after completion
+        if RequestType == 'Delete' or continues to (re)reate resource.'''
+    if RequestType in ['Update', 'Delete']:
+        try:
+            responseData = handle_event(agent, event, delete=True)
+            if RequestType == 'Delete':
+                cfnresponse.send(
+                    event,
+                    context,
+                    cfnresponse.SUCCESS,
+                    responseData=responseData,
+                    physicalResourceId=event['PhysicalResourceId']
+                )
+                return
+        except:
+            print_exc()
+            cfnresponse.send(
+                event,
+                context,
+                cfnresponse.FAILED
+            )
+            return
+
+
+    ''' Create: (re)creates a resource and returns PhysicalResourceId based on
+        the specified AgentResourceId.'''
+    try:
+        (PhysicalResourceId, responseData) = handle_event(agent, event, create=True)
+        cfnresponse.send(
+            event,
+            context,
+            cfnresponse.SUCCESS,
+            responseData=responseData,
+            physicalResourceId=PhysicalResourceId
+        )
+        return
+    except:
+        print_exc()
+        cfnresponse.send(
+            event,
+            context,
+            cfnresponse.FAILED
+        )
+        return
+
+
+if __name__ == '__main__':
+    event = json.loads(sys.argv[1])
+    lambda_handler(event=event)
