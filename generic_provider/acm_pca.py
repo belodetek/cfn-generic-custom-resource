64df5eee42c8ce7aa69d67e6f39782b2a9854cdb
Anton Belodedenko <abelodedenko@thrivepos.com>
2019-08-30 16:50:51 -0700

    acm_pca.py (WIP)

---
+++ generic_provider/acm_pca.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import os
+import re
+import random
+import boto3
+from OpenSSL import crypto
+from datetime import datetime
+
+
+class ACM_PCA:
+    def __init__(self, *args, **kwargs):
+        self.verbose = bool(int(os.getenv('VERBOSE', 0)))
+        if self.verbose: print('args: {}, kwargs: {}'.format(args, kwargs))
+
+
+    def get_private_key_pem(self, param):
+        ssm = boto3.client('ssm')
+        return ssm.get_parameter(
+            Name=param,
+            WithDecryption=True
+        )['Parameter']['Value']
+
+
+    def load_private_key(self, key_pem):
+        return crypto.load_privatekey(
+            crypto.FILETYPE_PEM,
+            bytes(key_pem, 'utf-8')
+        )
+
+
+    def dump_private_key(self, private_key):
+        return crypto.dump_privatekey(
+            crypto.FILETYPE_PEM,
+            private_key
+        )
+
+
+    def sign_csr(self, *args, **kwargs):
+        if self.verbose: print('args: {}, kwargs: {}'.format(args, kwargs))
+        self.private_key = self.load_private_key(
+            self.get_private_key_pem(kwargs['PrivateKey'])
+        )
+
+        csr_pem = kwargs['Csr']
+        try:
+            csr_payload = re.search(
+                '-----BEGIN CERTIFICATE REQUEST-----(.*)-----END CERTIFICATE REQUEST-----',
+                csr_pem,
+                re.IGNORECASE
+            )
+            assert csr_payload
+            csr_formatted = '{}{}{}'.format(
+                '-----BEGIN CERTIFICATE REQUEST-----',
+                csr_payload.group(1).replace(' ', '\n'),
+                '-----END CERTIFICATE REQUEST-----'
+            )
+            csr_pem = csr_formatted
+        except:
+            csr = crypto.load_certificate_request(
+                crypto.FILETYPE_PEM,
+                csr_pem
+            )
+
+        cert = crypto.X509()
+        cert.set_version(2)
+        cert.set_serial_number(random.randint(50000000,100000000))
+        cert.set_subject(csr.get_subject())
+        cert.set_pubkey(csr.get_pubkey())
+        cert.add_extensions([
+            crypto.X509Extension(
+                bytes('basicConstraints', 'utf-8'),
+                False,
+                bytes('critical,CA:TRUE', 'utf-8'),
+            )
+        ])
+        cert.gmtime_adj_notBefore(0)
+        cert.gmtime_adj_notAfter(kwargs['ValidityInSeconds'])
+        cert.sign(self.private_key, kwargs['Digest'])        
+        return {
+            'Certificate': crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode('utf-8')
+        }
+
+
+    def import_certificate_authority_certificate(self, *args, **kwargs):
+        if self.verbose: print('args: {}, kwargs: {}'.format(args, kwargs))
+        client = boto3.client('acm-pca')
+        params = {
+            'CertificateAuthorityArn': kwargs['CertificateAuthorityArn'],
+            'Certificate': bytes(kwargs['Certificate'], 'utf-8')
+        }
+        try:
+            assert 'CertificateChain' in kwargs
+            params['CertificateChain'] = bytes(kwargs['CertificateChain'], 'utf-8')
+        except:
+            pass
+        return client.import_certificate_authority_certificate(params)
